a6e55097ef8cdc6e88af35a08a28a7194e02a3df
tayediggs <61007777+tayediggs@users.noreply.github.com>
2020-04-20 06:38:23 +0100

    javatask 3
    javascript task 3 done

--- javatask3/script.js
+++ javatask3/script.js
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>	
+    <title>Document</title>
+</head>
+<body>
+	javascript task 3
+<script>
 function output(string) {
 	let strArr = [];
 	for (let i\= 1; i <= string; i++) {
@@ -32,3 +42,8 @@ function output(string) {
 }
 console.log(output(30));
 console.log(output(10));
+
+
+	</script>
+</body>
+</html>
